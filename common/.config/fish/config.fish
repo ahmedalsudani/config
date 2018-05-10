54c665cd6f043d3c0e7ee2814ff6c159075a5665
Ahmed Al-Sudani <git@ahmed.al-sudani.com>
2018-05-10 15:09:24 -0500

    Some fish config for work

--- common/.config/fish/config.fish
+++ common/.config/fish/config.fish
@@ -1,6 +1,11 @@
-. ~/.env
+# . ~/.env
 
-set -gx PATH ~/bin $PATH
+if test (tty) = '/dev/tty2' -a -z "$TOP_FISH"
+    setenv TOP_FISH '1'
+    zsh -c '. ~/.profile && fish'; and exit
+end
+
+set -gx PATH $PATH ~/bin
 
 if not ssh-add -l >/dev/null 2>&1; and test -e ~/.ssh/id_rsa
     ssh-add ~/.ssh/id_rsa
