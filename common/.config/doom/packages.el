f78eb7d61b5a9b2640efa5435ca5c352e05655f2
Ahmed Al-Sudani <aalsudani@nvidia.com>
2020-11-02 23:57:16 -0600

    Add ztree in doom/packages

--- common/.config/doom/packages.el
+++ common/.config/doom/packages.el
@@ -10,6 +10,7 @@
 ;(package! some-package)
 (package! ag)
 (package! key-chord)
+(package! ztree)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
