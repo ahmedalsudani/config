fa19e7b942bfd5a2c6c58df05cafbc6549430f25
Ahmed Al-Sudani <aalsudani@nvidia.com>
2021-04-28 11:26:11 -0500

    Doom config updates

--- common/.config/doom/config.el
+++ common/.config/doom/config.el
@@ -25,7 +25,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one-light)
+;(setq doom-theme 'doom-opera-light-theme)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -51,6 +51,8 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(remove-hook 'doom-first-buffer-hook #'global-hl-line-mode)
 
 (key-chord-define evil-insert-state-map "kj" 'evil-normal-state)
 (key-chord-define evil-insert-state-map "df" 'evil-normal-state)
--- common/.config/doom/custom.el
+++ common/.config/doom/custom.el
@@ -4,10 +4,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("99ea831ca79a916f1bd789de366b639d09811501e8c092c85b2cb7d697777f93" "990e24b406787568c592db2b853aa65ecc2dcd08146c0d22293259d400174e37" default)))
+   '("93ed23c504b202cf96ee591138b0012c295338f38046a1f3c14522d4a64d7308" "99ea831ca79a916f1bd789de366b639d09811501e8c092c85b2cb7d697777f93" "990e24b406787568c592db2b853aa65ecc2dcd08146c0d22293259d400174e37" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'projectile-ag 'disabled nil)
