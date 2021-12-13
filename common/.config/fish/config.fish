c197715ce4823e6c2d54d03589e289763387f765
Ahmed Al-Sudani <git@ahmed.al-sudani.com>
2021-12-13 17:02:32 -0600

    Update doom, doom foont size: 18, npm vars

--- common/.config/fish/config.fish
+++ common/.config/fish/config.fish
@@ -7,3 +7,7 @@
 . ~/.opam/opam-init/init.fish > /dev/null 2> /dev/null or true
 
 set -gx EDITOR "ec -c"
+
+set NPM_PACKAGES "$HOME/.npm-packages"
+set PATH $PATH $NPM_PACKAGES/bin
+set MANPATH $NPM_PACKAGES/share/man $MANPATH
