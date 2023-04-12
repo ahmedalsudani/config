ff0371f8aeff6e6fc486f7b5c1faa9f2d6a69ab2
Ahmed Al-Sudani <aalsudani@nvidia.com>
2023-04-12 11:24:22 -0500

    Remove a few lines from fish config

--- common/.config/doom/packages.el
+++ common/.config/doom/packages.el
@@ -13,6 +13,7 @@
 (package! ztree)
 (package! p4)
 (package! iedit)
+(package! cmake-ide)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
--- common/.config/fish/config.fish
+++ common/.config/fish/config.fish
@@ -5,9 +5,3 @@
 
 # OPAM configuration
 . ~/.opam/opam-init/init.fish > /dev/null 2> /dev/null or true
-
-#set -gx EDITOR "ec -c"
-
-set NPM_PACKAGES "$HOME/.npm-packages"
-set PATH $PATH $NPM_PACKAGES/bin
-set MANPATH $NPM_PACKAGES/share/man $MANPATH
