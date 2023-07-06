84e6b72d3910bab0b2d4fc5d45497df953b3a6c3
Ahmed Al-Sudani <code@ahmed.al-sudani.com>
2023-07-05 22:24:05 -0400

    ghcup, nvm, emacs cmake-mode

--- common/.config/fish/config.fish
+++ common/.config/fish/config.fish
@@ -7,3 +7,10 @@
 . ~/.opam/opam-init/init.fish > /dev/null 2> /dev/null or true
 
 set -gx EDITOR vim
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/user/.ghcup/bin # ghcup-env
+
+function nvm
+  bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
+end
+
